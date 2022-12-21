a6fed0fe5309f48fd67a2f83ad12d47eee7c5e8d
tkddntj <109967208+tkddntj@users.noreply.github.com>
2022-12-22 02:00:03 +0900

    Update Mediapipe_Save_CSV.py

--- Mediapipe_Save_CSV.py
+++ Mediapipe_Save_CSV.py
@@ -83,10 +83,7 @@ cap = cv2.VideoCapture(0)
 # Initiate holistic model
 
 def calculate_angle(a,b,c):
-    a = np.array(a) # First
-    b = np.array(b) # Mid
-    c = np.array(c) # End
-    
+
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
     
